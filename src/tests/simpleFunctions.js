b0d64b65f38b20f75ab8cd63e05a1c838edf02fc
Valentin Popov <info@valentineus.link>
2017-09-19 14:54:29 +0400

    Adding a description

--- src/tests/simpleFunctions.js
+++ src/tests/simpleFunctions.js
@@ -1,19 +1,24 @@
 import { Manager, Channel } from '../lib/index';
 
 /**
- * @protected
  * @description Magic function generating a random true.
  */
 function trueFunction() {
     return true;
 }
 
+/**
+ * @description Create the layout of the connected channel
+ */
 function simpleChannel() {
     var channel = new Channel(trueFunction, trueFunction, trueFunction, trueFunction);
     channel.connected();
     return channel;
 }
 
+/**
+ * @description Create a layout for the channel manager
+ */
 function simpleManager() {
     return new Manager(trueFunction, trueFunction, trueFunction);
 }
