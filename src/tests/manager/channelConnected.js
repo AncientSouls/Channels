0af3c3a9f3e22fe50eba5c41d94c74eeaeb67356
Valentin Popov <info@valentineus.link>
2017-09-19 11:25:21 +0400

    Some fixes to the testing function 'channelConnected'

--- src/tests/manager/channelConnected.js
+++ src/tests/manager/channelConnected.js
@@ -1,127 +1,97 @@
 import { assert } from 'chai';
 import sinon from 'sinon';
 
-import { Manager, Channel } from '../../lib/index';
-
-/**
- * @protected
- * @description Magic function generating a random true.
- */
-function trueFunction() { return true; }
+import { simpleManager, simpleChannel } from '../simpleFunctions';
+import { Manager } from '../../lib/index';
 
 export default function () {
-    describe('channelConnected():', () => {
+    describe('_channelConnected():', () => {
         describe('Argument:', () => {
             it('Channel', () => {
-                /* Create a channel */
-                var channel = new Channel(trueFunction, null, null, null);
-                channel.connected();
-                /* Creating a manager */
-                var manager = new Manager(trueFunction, null, null);
-                /* Checking */
-                assert.doesNotThrow(() => manager.channelConnected(channel));
+                var channel = simpleChannel();
+                var manager = simpleManager();
+                assert.doesNotThrow(() => manager._channelConnected(channel));
             });
 
             describe('Exceptions:', () => {
                 it('Boolean', () => {
-                    var manager = new Manager(trueFunction, null, null);
-                    assert.throws(() => manager.channelConnected(false));
-                    assert.throws(() => manager.channelConnected(true));
+                    var manager = simpleManager();
+                    assert.throws(() => manager._channelConnected(false));
+                    assert.throws(() => manager._channelConnected(true));
                 });
 
                 it('Null', () => {
-                    var manager = new Manager(trueFunction, null, null);
-                    assert.throws(() => manager.channelConnected(null));
+                    var manager = simpleManager();
+                    assert.throws(() => manager._channelConnected(null));
                 });
 
                 it('Undefined', () => {
-                    var manager = new Manager(trueFunction, null, null);
-                    assert.throws(() => manager.channelConnected(undefined));
+                    var manager = simpleManager();
+                    assert.throws(() => manager._channelConnected(undefined));
                 });
 
                 it('Number', () => {
-                    var manager = new Manager(trueFunction, null, null);
-                    assert.throws(() => manager.channelConnected(1234567890));
+                    var manager = simpleManager();
+                    assert.throws(() => manager._channelConnected(1234567890));
                 });
 
                 it('String', () => {
                     var text = 'Trust Your Doctor';
-                    var manager = new Manager(trueFunction, null, null);
-                    assert.throws(() => manager.channelConnected(text));
+                    var manager = simpleManager();
+                    assert.throws(() => manager._channelConnected(text));
                 });
 
                 it('Object', () => {
-                    var manager = new Manager(trueFunction, null, null);
-                    assert.throws(() => manager.channelConnected({}));
+                    var manager = simpleManager();
+                    assert.throws(() => manager._channelConnected({}));
                 });
 
                 it('Function', () => {
-                    var manager = new Manager(trueFunction, null, null);
-                    assert.throws(() => manager.channelConnected(() => {}));
+                    var manager = simpleManager();
+                    assert.throws(() => manager._channelConnected(() => {}));
                 });
             });
         });
 
         describe('Storage:', () => {
             it('Addition', () => {
-                /* Create a channel */
-                var channel = new Channel(trueFunction, null, null, null);
-                channel.connected();
-                /* Creating a manager */
-                var manager = new Manager(trueFunction, null, null);
-                manager.channelConnected(channel);
-                /* Checking */
+                var channel = simpleChannel();
+                var manager = simpleManager();
+                manager._channelConnected(channel);
                 assert.equal(channel.id, manager.channels[channel.id]);
             });
 
             it('Duplication', () => {
-                var fake = { test: 'A change is going to come...' };
-                /* Create a channel */
-                var channel = new Channel(trueFunction, null, null, null);
-                channel.connected();
-                /* Creating a manager */
-                var manager = new Manager(trueFunction, null, null);
-                manager.channels[channel.id] = fake;
-                manager.channelConnected(channel);
-                /* Checking */
-                assert.notEqual(fake, manager.channels[channel.id]);
+                var channel = simpleChannel();
+                var manager = simpleManager();
+                manager._channelConnected(channel);
+                manager._channelConnected(channel);
+                assert.equal(channel, manager.channels[channel.id]);
                 assert.lengthOf(manager.channels, 1);
             });
 
             it('Exception', () => {
-                /* Create a channel */
-                var channel = new Channel(trueFunction, null, null, null);
-                channel.connected();
-                /* Creating a manager */
-                var manager = new Manager(trueFunction, null, null);
+                var channel = simpleChannel();
+                var manager = simpleManager();
                 manager.channels = null;
-                /* Checking */
-                assert.throws(() => manager.channelConnected(channel));
+                assert.doesNotThrow(() => manager._channelConnected(channel));
             });
         });
 
         describe('onConnected():', () => {
             it('Function call', () => {
-                /* Create a channel */
-                var channel = new Channel(trueFunction, null, null, null);
-                channel.connected();
-                /* Creating a manager */
                 var spy = sinon.spy();
+                var channel = simpleChannel();
                 var manager = new Manager(spy, null, null);
-                manager.channelConnected(channel);
-                /* Checking */
+                manager._channelConnected(channel);
                 assert.isTrue(spy.called);
             });
 
             it('Parameter transfer', () => {
-                /* Create a channel */
-                var channel = new Channel(trueFunction, null, null, null);
-                channel.connected();
-                /* Creating a manager */
                 var spy = sinon.spy();
+                var channel = simpleChannel();
                 var manager = new Manager(spy, null, null);
-                manager.channelConnected(channel);
-                /* Checking */
+                manager._channelConnected(channel);
                 assert.isTrue(spy.calledWith(channel));
             });
         });
