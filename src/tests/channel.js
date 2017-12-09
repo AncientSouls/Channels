d55d9f0182bf85bb9c824f513b3be2ee85dcd1d4
Valentin Popov <info@valentineus.link>
2017-12-10 01:56:10 +0400

    Refactoring tests by channels

--- src/tests/channel.js
+++ src/tests/channel.js
@@ -1,121 +1,81 @@
 import { assert } from 'chai';
 import crypto from 'crypto';
-import sinon from 'sinon';
 
 import { Channel } from '../lib/index';
-
-function sendPackage(channel, pkg) {
-    channel.got(pkg);
-}
-
-function generatorString() {
-    return crypto.randomBytes(20).toString('hex');
-}
 
 export default function () {
     describe('Channel:', () => {
         describe('Connect / Disconnect:', () => {
-            var channel = null;
+            it('connect(true) / connected', (done) => {
+                var channel = new Channel(
+                    function onConnected(channel) {
+                        assert.isTrue(channel.isConnected);
+                        assert.isString(channel.sharedKey);
+                        done();
+                    },
+                    function onDisconnected(channel) {},
+                    function gotPackage(channel, data) {},
+                    function sendPackage(channel, pkg) {
+                        channel.got(pkg);
+                    }
+                );
 
-            beforeEach(() => {
-                channel = new Channel(sinon.spy(), sinon.spy(), null, sendPackage);
-            });
-
-            it('connected()', () => {
-                channel.connected();
-
-                assert.isTrue(channel.onConnected.calledWithExactly(channel));
-                assert.isTrue(channel.isConnected);
-                assert.isNotNull(channel.id);
-            });
-
-            it('disconnected()', () => {
-                channel.connected();
-                channel.disconnected();
-
-                assert.isTrue(channel.onDisconnected.calledWithExactly(channel));
-                assert.isFalse(channel.isConnected);
-            });
-
-            it('connect(true)', () => {
-                channel.connected = sinon.spy();
                 channel.connect(true);
-
-                assert.isString(channel.sharedKey);
-                assert.isTrue(channel.connected.called);
             });
 
-            it('connect(false)', () => {
-                channel.connected = sinon.spy();
+            it('connect(false) / connected', (done) => {
+                var channel = new Channel(
+                    function onConnected(channel) {
+                        assert.isTrue(channel.isConnected);
+                        assert.isNull(channel.sharedKey);
+                        done();
+                    },
+                    function onDisconnected(channel) {},
+                    function gotPackage(channel, data) {},
+                    function sendPackage(channel, pkg) {
+                        channel.got(pkg);
+                    }
+                );
+
                 channel.connect(false);
-
-                assert.isNull(channel.sharedKey);
-                assert.isTrue(channel.connected.called);
             });
 
-            it('disconnect()', () => {
-                channel.disconnected = sinon.spy();
-                channel.disconnect();
+            it('disconnect() / disconnected', (done) => {
+                var channel = new Channel(
+                    function onConnected(channel) {},
+                    function onDisconnected(channel) {
+                        assert.isFalse(channel.isConnected);
+                        done();
+                    },
+                    function gotPackage(channel, data) {},
+                    function sendPackage(channel, pkg) {}
+                );
 
-                assert.isFalse(channel.isConnected);
-                assert.isTrue(channel.disconnected.called);
+                channel.disconnect();
             });
         });
 
         describe('Sending / Receiving:', () => {
-            var channel = null;
-            var pkg = null;
-
-            beforeEach(() => {
-                channel = new Channel(null, null, sinon.spy(), sendPackage);
-                pkg = generatorString();
-            });
-
-            it('Loopback', () => {
-                channel.send(pkg);
-                assert.isTrue(channel.gotPackage.calledWithExactly(channel, pkg));
-            });
-
-            it('Send encrypted', () => {
-                var original = channel._createPackage;
-
-                sinon.stub(channel, '_createPackage').callsFake(original);
-                channel.connect(true);
-                channel.send(pkg);
-
-                assert.isTrue(channel._createPackage.neverCalledWith(pkg));
-            });
-
-            it('Send unencrypted', () => {
-                var original = channel._createPackage;
-
-                sinon.stub(channel, '_createPackage').callsFake(original);
-                channel.connect(false);
-                channel.send(pkg);
-
-                assert.isTrue(channel._createPackage.calledWithExactly(pkg));
-            });
-        });
-
-        describe('Example:', () => {
-            it('Local transport', (done) => {
-                var pkg = generatorString();
+            it('Send encrypted', (done) => {
+                var pkg = {
+                    text: crypto.randomBytes(20).toString('hex')
+                };
 
                 /* First channel */
-                var client = new Channel(
+                var channel_1 = new Channel(
                     function onConnected(channel) {},
                     function onDisconnected(channel) {},
                     function gotPackage(channel, data) {
-                        assert.equal(pkg, data);
+                        assert.deepEqual(pkg, data);
                         done();
                     },
                     function sendPackage(channel, pkg) {
-                        server.got(pkg);
+                        channel_2.got(pkg);
                     }
                 );
 
                 /* Second channel */
-                var server = new Channel(
+                var channel_2 = new Channel(
                     function onConnected(channel) {
                         channel.connect(true);
                     },
@@ -124,15 +84,54 @@ export default function () {
                         channel.send(data);
                     },
                     function sendPackage(channel, pkg) {
-                        client.got(pkg);
+                        channel_1.got(pkg);
                     }
                 );
 
-                /* Reconciliation of channels */
-                client.connect(true);
+                /* Coordinate the channels */
+                channel_1.connect(true);
 
                 /* Data transfer */
-                client.send(pkg);
+                channel_1.send(pkg);
+            });
+
+            it('Send unencrypted', (done) => {
+                var pkg = {
+                    text: crypto.randomBytes(20).toString('hex')
+                };
+
+                /* First channel */
+                var channel_1 = new Channel(
+                    function onConnected(channel) {},
+                    function onDisconnected(channel) {},
+                    function gotPackage(channel, data) {
+                        assert.deepEqual(pkg, data);
+                        done();
+                    },
+                    function sendPackage(channel, pkg) {
+                        channel_2.got(pkg);
+                    }
+                );
+
+                /* Second channel */
+                var channel_2 = new Channel(
+                    function onConnected(channel) {
+                        channel.connect(false);
+                    },
+                    function onDisconnected(channel) {},
+                    function gotPackage(channel, data) {
+                        channel.send(data);
+                    },
+                    function sendPackage(channel, pkg) {
+                        channel_1.got(pkg);
+                    }
+                );
+
+                /* Coordinate the channels */
+                channel_1.connect(false);
+
+                /* Data transfer */
+                channel_1.send(pkg);
             });
         });
     });
