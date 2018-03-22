e6d9be2a664a1c9260182feb8392f13161f93906
Ivan S Glazunov <ivansglazunov@gmail.com>
2018-03-23 01:34:54 +0300

    fix events typing

--- lib/channels-manager.d.ts
+++ lib/channels-manager.d.ts
@@ -3,6 +3,11 @@ import { IManager, IManagerEventsList } from 'ancient-mixins/lib/manager';
 import { IChannelPkgEventData, TChannel } from './channel';
 declare type TChannelsManager = IChannelsManager<TChannel, IChannelsManagerEventsList>;
 interface IChannelsManagerEventData extends IChannelPkgEventData {
+    node?: TChannel;
+    manager: TChannelsManager;
+}
+interface IChannelsManagerEventManagerData {
+    node: TChannel;
     manager: TChannelsManager;
 }
 interface IChannelsManagerEventsList extends IManagerEventsList {
@@ -14,6 +19,8 @@ interface IChannelsManagerEventsList extends IManagerEventsList {
     send: IChannelsManagerEventData;
     pack: IChannelsManagerEventData;
     unpack: IChannelsManagerEventData;
+    added: IChannelsManagerEventManagerData;
+    removed: IChannelsManagerEventManagerData;
 }
 interface IChannelsManager<IN extends TChannel, IEventsList extends IChannelsManagerEventsList> extends IManager<IN, IEventsList> {
 }
@@ -21,4 +28,4 @@ declare function mixin<T extends TClass<IInstance>>(superClass: T): any;
 declare const MixedChannelsManager: TClass<TChannelsManager>;
 declare class ChannelsManager extends MixedChannelsManager {
 }
-export { mixin as default, mixin, MixedChannelsManager, ChannelsManager, IChannelsManager, IChannelsManagerEventData, IChannelsManagerEventsList, TChannelsManager };
+export { mixin as default, mixin, MixedChannelsManager, ChannelsManager, IChannelsManager, IChannelsManagerEventData, IChannelsManagerEventManagerData, IChannelsManagerEventsList, TChannelsManager };
