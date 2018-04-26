6115b2bd60a93aa7a1bd606ac76b217fef6205bf
Menzorg <menzorg@gmail.com>
2018-04-27 00:08:11 +0400

    delete sinon from package.json and delete channal-manager

--- src/tests/index.ts
+++ src/tests/index.ts
@@ -1,11 +1,9 @@
 require('source-map-support').install();
 
 import channel from './channel';
-import channelsManager from './channels-manager';
 import createLocalTransport from './create-local-transport';
 
 describe('AncientSouls/Channels:', () => {
   channel();
-  channelsManager();
   createLocalTransport();
 });
--- tests/index.js
+++ tests/index.js
@@ -2,11 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 require('source-map-support').install();
 const channel_1 = require("./channel");
-const channels_manager_1 = require("./channels-manager");
 const create_local_transport_1 = require("./create-local-transport");
 describe('AncientSouls/Channels:', () => {
     channel_1.default();
-    channels_manager_1.default();
     create_local_transport_1.default();
 });
 //# sourceMappingURL=index.js.map
