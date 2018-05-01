63fb40b56539d17c2c70ce22dea1899d4e69f562
Menzorg <menzorg@gmail.com>
2018-05-01 09:07:03 +0400

    Docs for local transport

--- src/lib/create-local-transport.ts
+++ src/lib/create-local-transport.ts
@@ -3,6 +3,9 @@ import {
 	IChannelEventsList,
 } from './channel';
 
+/**
+ * Two-side transport for channels. `Ready` event from one side emmits getting data for other side. It looks like transport for sockets.
+ */
 function createLocalTransport(
 	channel1: IChannel<IChannelEventsList>,
 	channel2: IChannel<IChannelEventsList>,
