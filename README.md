fb174ede1d51abfd3eb613ac318945211873cd14
Menzorg <menzorg@gmail.com>
2018-05-01 08:00:02 +0400

    First doc

--- README.md
+++ README.md
@@ -6,3 +6,53 @@ Abstract modular channels concept.
 [![Build Status](https://travis-ci.org/AncientSouls/Channels.svg?branch=master)](https://travis-ci.org/AncientSouls/Channels)
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/b78123b90d434029b9a9a6d2089b17b8)](https://www.codacy.com/app/ivansglazunov/Channels?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=AncientSouls/Channels&amp;utm_campaign=Badge_Grade)
 [![Read the Docs](https://img.shields.io/readthedocs/pip.svg)](https://ancientsouls.github.io/)
+
+## Install
+
+```
+npm i --save ancient-channel
+```
+## About 
+
+Usually channels, sockets or queries send data when it is ready. That means, when connection does not exist, data for sending need to be stacked or ignored. We divided this process by three stages:
+1. `channel.ready()`. The moment when the need / possibility to send data became known. This should not necessarily means that we know what data is or sending should be triggered. If the communication architecture allows you to notify other side immediately - the second stage will start immediately. With a one-side or not stable connection, this can happen, for example, when `channel.get()` called.
+2. `channel.get()` The moment when we can send some data. Transport environment asks what you can / need to send now.
+3. `channe.got()` On the other side of the channel, the transport environment reports that some data has been received.
+
+## Example
+
+```js
+import {
+  Channel,
+} from 'ancient-channel/lib/channel';
+
+import createLocalTransport from 'ancient-channel/lib/create-local-transport';
+
+const c1 = new Channel();
+const c2 = new Channel();
+
+const data = { num: 123 };
+
+let c1got, c2got;
+const getter = () => data;
+c1.getter = c2.getter = getter;
+
+createLocalTransport(c1, c2);
+
+c1.on('get', ({ channel, data }) => data.num += data.num);
+c2.once('got', ({ channel, data }) => {
+  c2got = data.num === 246;
+  channel.ready();
+});
+
+c1.once('got', ({ channel, data }) => {
+  c1got = data.num === 246;
+});
+
+c1got; // undefined
+c2got; // undefined
+
+c1.ready();
+c1got; // true
+c2got; // true
+```
--- src/lib/channel.ts
+++ src/lib/channel.ts
@@ -12,8 +12,19 @@ import {
 export type TChannel = IChannel<IChannelEventsList>;
 
 export interface IChannelEventsList extends INodeEventsList {
+  /**
+   * Some new data have gotten.
+   */
   got: { data: any; channel: TChannel; };
+
+  /**
+   * Channel is ready to to getting.
+   */
   ready: { channel: TChannel; };
+
+  /**
+   * Channel asked new data.
+   */
   get: { data: any; channel: TChannel; };
 }
 
@@ -22,14 +33,56 @@ export interface IChannelGetter<result extends any> {
 }
 
 export interface IChannel<IEventsList extends IChannelEventsList> extends INode<IEventsList> {
-  isReading: boolean;
+
+  /**
+   * Is data ready to getting / exists some data need to be send.
+   */
+  isReady: boolean;
+
+  /**
+   * Function, prepears data for `get()`.
+   * @example
+   * ```typescript
+   * 
+   * import {
+   *   Channel,
+   * } from 'ancient-channel/lib/channel';
+   * 
+   * const c = new Channel();
+   * c.getter = () => 123
+   * await c.get(); // undefined
+   * c.ready();
+   * await c.get(); // 123
+   * await c.get(); // undefined
+   * ```
+   */
   getter?: IChannelGetter<any>;
 
+  /**
+   * Emit `got` event about new data.
+   */
   got(data?: Promise<any>|any): void;
+  /**
+   * Emit `ready` event about reading for getting.
+   */
   ready(): void;
+  /**
+   * Emit `get` event for asking new data.
+   */
   get(): Promise<any>;
 }
 
+/**
+ * Mixin your Node with Channel functionality.
+ * @example
+ * ```typescript
+ * 
+ * import { mixin, TChannel } from 'ancient-channel/lib/channel';
+ * import { Node } from 'ancient-mixins/lib/node';
+ * import { TClass } from 'ancient-mixins/lib/mixins';
+ * const MixedManager: TClass<TChannel> = mixin(Node);
+ * ```
+ */
 export function mixin<T extends TClass<IInstance>>(
   superClass: T,
 ): any {
@@ -42,15 +95,15 @@ export function mixin<T extends TClass<IInstance>>(
     }
     
     ready() {
-      this.isReading = true;
+      this.isReady = true;
       this.emit('ready', { channel: this });
     }
     
     async get() {
       let data = undefined;
-      if (this.isReading) {
+      if (this.isReady) {
         data = await this.getter();
-        this.isReading = false;
+        this.isReady = false;
       }
       this.emit('get', { data, channel: this });
       return data;
@@ -59,4 +112,7 @@ export function mixin<T extends TClass<IInstance>>(
 }
 
 export const MixedChannel: TClass<IChannel<IChannelEventsList>> = mixin(Node);
+/**
+ * Already mixed class. Plug and play.
+ */
 export class Channel extends MixedChannel {}
